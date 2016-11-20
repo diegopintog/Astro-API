0f16354661c63b000f269e00c4ed334ba0310d3f
Diego Pinto Guerra <Diego Pinto Guerra>
2016-11-20 16:04:53 -0500

    BodyPosition class created: This class builds the body data for the USER/Julianday given

--- astroAPI/src/main/java/com/astro/astroAPI/YourFirstAPI.java
+++
@@ -1,8 +0,0 @@
-package com.astro.astroAPI;
-
-/**
-  * Add your first API methods in this class, or you may create another class. In that case, please
-  * update your web.xml accordingly.
- **/
-public class YourFirstAPI {
-}
---
+++ astroAPI/src/main/java/com/astro/astroAPI/forecastCalculation.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.astro.astroAPI;
+
+/**
+ * @author diego
+ *
+ */
+public class forecastCalculation {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
