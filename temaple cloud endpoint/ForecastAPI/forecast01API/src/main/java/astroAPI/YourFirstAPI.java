fbd5cf3c21985ee8bd224c67f87e938f7f663c1d
Diego Pinto Guerra <Diego Pinto Guerra>
2016-11-20 15:04:40 -0500

    Maven cloud endpoint creation, with skeleton template.

---
+++ temaple cloud endpoint/ForecastAPI/forecast01API/src/main/java/astroAPI/YourFirstAPI.java
@@ -0,0 +1,8 @@
+package astroAPI;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+public class YourFirstAPI {
+}
