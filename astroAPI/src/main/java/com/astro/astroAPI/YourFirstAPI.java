a17dfa3d50768d31f06259fe21b17317d079715a
diegopintog <diegopintog@outlook.com>
2016-11-20 20:22:00 -0500

    mvn clean install of endpoints-skeleton-archetype

---
+++ astroAPI/src/main/java/com/astro/astroAPI/YourFirstAPI.java
@@ -0,0 +1,8 @@
+package com.astro.astroAPI;
+
+/**
+  * Add your first API methods in this class, or you may create another class. In that case, please
+  * update your web.xml accordingly.
+ **/
+public class YourFirstAPI {
+}
