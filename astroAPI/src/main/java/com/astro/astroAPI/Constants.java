655667833422e1c08f743387a8a4a9e639780ced
diegopintog <diegopintog@outlook.com>
2016-11-20 20:36:15 -0500

    Basic service structure

--- astroAPI/src/main/java/com/astro/astroAPI/Constants.java
+++ astroAPI/src/main/java/com/astro/astroAPI/Constants.java
@@ -8,6 +8,7 @@ public class Constants {
   public static final String ANDROID_CLIENT_ID = "replace this with your Android client ID";
   public static final String IOS_CLIENT_ID = "replace this with your iOS client ID";
   public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
+  public static final String API_EXPLORER_CLIENT_ID = com.google.api.server.spi.Constant.API_EXPLORER_CLIENT_ID;
 
   public static final String EMAIL_SCOPE = "https://www.googleapis.com/auth/userinfo.email";
 }
--- astroAPI/src/main/java/com/astro/astroAPI/ForecastCalculation.java
+++ astroAPI/src/main/java/com/astro/astroAPI/ForecastCalculation.java
@@ -3,6 +3,20 @@
  */
 package com.astro.astroAPI;
 
+import com.google.api.server.spi.config.Api;
+import com.google.api.server.spi.config.ApiMethod;
+import com.google.api.server.spi.config.Named;
+import com.google.api.server.spi.response.NotFoundException;
+
+@Api(
+	    name        = "ForecastWebServiceTemaplate",
+	    version     = "v1",
+	    title       = "Forecast Backend API",
+	    description = "Forecasting core services",
+	    clientIds   = {Constants.WEB_CLIENT_ID, Constants.API_EXPLORER_CLIENT_ID , Constants.ANDROID_CLIENT_ID, Constants.IOS_CLIENT_ID},
+	    audiences   = {Constants.ANDROID_AUDIENCE}
+	)
+
 /**
  * @author diegopintog
  *
@@ -12,9 +26,17 @@ public class ForecastCalculation {
 	/**
 	 * @param args
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
+	@ApiMethod(httpMethod = "GET", path = "getForecast")
+	public String getForecastApp(@Named("id") String id) throws NotFoundException{
+		try {  
+			  
+			String test = "";
+			    
+			return test; 
+		    
+		  } catch (IndexOutOfBoundsException e) {
+		      throw new NotFoundException("forecast not found with an index: " + id);
+		  }    
+	  } 
 
 }
